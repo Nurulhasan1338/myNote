9a94c3c40f84011fa2d870ece72561d8c4e3850f
Nurul Hasan <2020kucp1109@iiitkota.ac.in>
2023-04-14 12:17:49 +0530

    add gitIgnore

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import Navbar from "./component/navbar.js";
-import Home from "./component/Home.js";
-import Alert from "./component/Alert.js"
-import About from "./component/About.js";
-import NoteState from './component/context/Notes/Notesate';
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
-
-
 
 function App() {
   return (
     <div className="App">
-      <NoteState>
-        <BrowserRouter>
-              <Navbar/>
-        <Alert/>
-              <div className="container">   
-               <Routes>
-              <Route path="/" element ={<Home/>}/>
-              <Route path="/about" element ={<About/>}/>
-            </Routes>
-            </div>
-
-        </BrowserRouter>
-      </NoteState>
- </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
